a99fbf8711fefe73369501e5393d4c699fafdd13
rayanMishra <rayanmishra7@gmail.com>
2023-05-24 12:36:37 -0400

    made slight changes to the app functionailty

--- src/Pages/GameRoom.js
+++ src/Pages/GameRoom.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
@@ -10,6 +9,8 @@ const GameRoom = ({ category, difficulty, type }) => {
   const [score, setScore] = useState(0);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
+  const [quizCompleted, setQuizCompleted] = useState(false);
+  // console.log(category, difficulty, type);
 
   useEffect(() => {
     fetchData(category, difficulty, type);
@@ -17,17 +18,21 @@ const GameRoom = ({ category, difficulty, type }) => {
 
   const fetchData = (category, difficulty, type) => {
     axios
-      .get(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=${type}`)
-      .then(response => {
-        const questionsWithUniqueIds = response.data.results.map(question => ({
-          ...question,
-          id: uuidv4(),
-        }));
+      .get(
+        `https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=${type}`
+      )
+      .then((response) => {
+        const questionsWithUniqueIds = response.data.results.map(
+          (question) => ({
+            ...question,
+            id: uuidv4(),
+          })
+        );
         setQuestions(questionsWithUniqueIds);
         setLoading(false);
         console.log(questionsWithUniqueIds);
       })
-      .catch(error => {
+      .catch((error) => {
         setError('Failed to fetch questions. Please try again later.');
         setLoading(false);
         console.log(error);
@@ -35,7 +40,13 @@ const GameRoom = ({ category, difficulty, type }) => {
   };
 
   const handleAnswer = (answer) => {
-    if (answer === questions[currentQuestion].correct_answer) {
+    if (quizCompleted) {
+      return; // Don't update the score if the quiz is completed
+    }
+
+    const currentQuestionObj = questions[currentQuestion];
+
+    if (answer === currentQuestionObj.correct_answer) {
       setScore(score + 1);
     }
 
@@ -44,31 +55,37 @@ const GameRoom = ({ category, difficulty, type }) => {
       setCurrentQuestion(nextQuestion);
     } else {
       // Quiz completed
+      // console.log('Quiz completed');
+      setQuizCompleted(true);
     }
   };
 
   if (loading) {
-    return (
-      <div>
-        {/* Loading... icon (react spinners package)*/}
-      </div>
-    );
+    return <div>{/* Loading... icon (react spinners package)*/}</div>;
   }
 
   if (error) {
     return (
       <div>
         {error}
-        <button onClick={ () => fetchData(category, difficulty, type)}>Retry</button>
+        <button onClick={() => fetchData(category, difficulty, type)}>
+          Retry
+        </button>
       </div>
     );
   }
+  // Rayan Added this for temporary erro hnading
+  if (questions.length === 0) {
+    return <p>Loading...</p>;
+  }
 
   const currentQuestionObj = questions[currentQuestion];
-  const options = [...currentQuestionObj.incorrect_answers, currentQuestionObj.correct_answer];
-  //Its going to shuffle options.We can call the sort() method, which accepts a function that returns a value between -0.5 and 0.5:
+  const options = [
+    ...currentQuestionObj.incorrect_answers,
+    currentQuestionObj.correct_answer,
+  ];
+  // //Its going to shuffle options.We can call the sort() method, which accepts a function that returns a value between -0.5 and 0.5:
   options.sort(() => Math.random() - 0.5);
-  
 
   return (
     <div>
@@ -78,10 +95,10 @@ const GameRoom = ({ category, difficulty, type }) => {
       <ul>
         {options.map((option) => {
           const optionId = uuidv4();
-          // console.log(`Option: ${option}, ID: ${optionId}`);
+          console.log(`Option: ${option}, ID: ${optionId}`);
           return (
             <li key={optionId} onClick={() => handleAnswer(option)}>
-              {option} 
+              {option}
             </li>
           );
         })}
@@ -91,7 +108,7 @@ const GameRoom = ({ category, difficulty, type }) => {
         // <Link to="/">
         //   <button>Finish Quiz</button>
         // </Link>
-        <Link to="/">Go to Home</Link> 
+        <Link to="/">Go to Home</Link>
       ) : null}
     </div>
   );
