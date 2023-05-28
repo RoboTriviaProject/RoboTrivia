38cdfd2eb5ed0d00446e35b2808062a539d8168b
rayanMishra <rayanmishra7@gmail.com>
2023-05-28 19:19:04 -0400

    solved merge conflicts by updating  gameroom functions

--- src/Pages/GameRoom.js
+++ src/Pages/GameRoom.js
@@ -74,171 +74,157 @@ const GameRoom = ({ category, difficulty, type, score, setScore }) => {
 
   // Handler for when an answer is selected
   const handleAnswer = (answer) => {
-    if (!quizCompleted && highlightAnswer) {
+    if (quizCompleted) {
+      navigate(`/gameroom/${gameId}/result`);
+      // Don't update the score if the quiz is completed or the highlighted answer is shown
+    }
+    if (highlightAnswer) {
       // Don't update the score if the quiz is completed or the highlighted answer is shown
       return;
-    } else if (quizCompleted) {
-      // go to results page
-      navigate(`/gameroom/${gameId}/result`);
+    }
 
-      const currentQuestionObj = questions[currentQuestion];
+    const currentQuestionObj = questions[currentQuestion];
 
-      if (answer === currentQuestionObj.correct_answer) {
-        setScore(score + 1);
-      }
+    if (answer === currentQuestionObj.correct_answer) {
+      setScore(score + 1);
+    }
 
-      // Clear timer and move to the next question immediately
-      if (resetTimer) {
-        resetTimer();
-      }
-
+    setHighlightAnswer(true);
+    setPaused(true);
+    setTimeout(() => {
+      setHighlightAnswer(false);
+      setPaused(false);
       const nextQuestion = currentQuestion + 1;
       if (nextQuestion < questions.length) {
         setCurrentQuestion(nextQuestion);
       } else {
-        // Quiz completed
         setQuizCompleted(true);
         navigate(`/gameroom/${gameId}/result`);
       }
-      setHighlightAnswer(true);
-      setPaused(true);
-      setTimeout(() => {
-        setHighlightAnswer(false);
-        setPaused(false);
-        const nextQuestion = currentQuestion + 1;
-        if (nextQuestion < questions.length) {
-          setCurrentQuestion(nextQuestion);
-        } else {
-          setQuizCompleted(true);
-        }
-      }, 3000);
-    }
+    }, 3000);
+  };
 
-    // Function to fetch data for the game
-    const fetchData = (category, difficulty, type) => {
-      // Reference to the game session data in Firebase
-      const gameSessionRef = ref(db, `gameSessions/${gameId}/questions`);
-      // Tries to Fetch the data from Firebase, if it exists then use it otherwise make API call and then save it
-      get(gameSessionRef)
-        .then((snapshot) => {
-          if (snapshot.exists()) {
-            const data = snapshot.val();
-            setQuestions(data);
-            setLoading(false);
-          } else {
-            // If the data does not exist in Firebase make the API call
-            axios
-              .get(
-                `https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=${type}`
-              )
-              .then((response) => {
-                const questionsWithUniqueIds = response.data.results.map(
-                  (question) => ({ ...question, id: uuidv4() })
-                );
-                // Save the data in Firebase
-                set(gameSessionRef, questionsWithUniqueIds)
-                  .then(() => {
-                    // Set the local state with the fetched data
-                    setQuestions(questionsWithUniqueIds);
-                    setLoading(false);
-                  })
-                  .catch((error) => {
-                    setError(
-                      'Failed to save questions to Firebase. Please try again later.'
-                    );
-                    setLoading(false);
-                  });
-              })
-              .catch((error) => {
-                setError('Failed to fetch questions. Please try again later.');
-                setLoading(false);
-              });
-          }
-        })
-        .catch((error) => {
-          setError(
-            'Failed to fetch data from Firebase. Please try again later.'
-          );
+  // Function to fetch data for the game
+  const fetchData = (category, difficulty, type) => {
+    // Reference to the game session data in Firebase
+    const gameSessionRef = ref(db, `gameSessions/${gameId}/questions`);
+    // Tries to Fetch the data from Firebase, if it exists then use it otherwise make API call and then save it
+    get(gameSessionRef)
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const data = snapshot.val();
+          setQuestions(data);
           setLoading(false);
-        });
-    };
-
-    // Rendering based on different states
-    if (loading) {
-      return (
-        <div className="spinnerContainer">
-          <PacmanLoader color="#fff" size={50} />
-        </div>
-      );
-    }
-
-    if (error) {
-      return (
-        <div>
-          {error}
-          <button onClick={() => fetchData(category, difficulty, type)}>
-            Retry
-          </button>
-        </div>
-      );
-    }
-    // Rendering based on different states
-    if (questions.length === 0) {
-      return <p>Loading...</p>;
-    }
-
-    const currentQuestionObj = questions[currentQuestion];
-    const options = [
-      ...currentQuestionObj.incorrect_answers,
-      currentQuestionObj.correct_answer,
-    ];
-    //Its going to shuffle options.We can call the sort() method, which accepts a function that returns a value between -0.5 and 0.5
-    //// options.sort(() => Math.random() - 0.5);
-
-    return (
-      <div>
-        <div className="quizInfo">
-          <p>Score: {score}/10</p>
-          <h3>{questions[currentQuestion].category}</h3>
-          <CountdownTimer
-            key={currentQuestion}
-            initialCount={15}
-            onExpire={handleExpire}
-            onReset={handleReset}
-            paused={paused}
-          />
-        </div>
-        <div className="questionFromAPI">
-          <h3>Question {currentQuestion + 1}</h3>
-          <p
-            dangerouslySetInnerHTML={{ __html: currentQuestionObj.question }}
-          />
-          <ul>
-            {options.map((option) => {
-              const optionId = uuidv4();
-              const isCorrectAnswer =
-                highlightAnswer && option === currentQuestionObj.correct_answer;
-              return (
-                <li
-                  key={optionId}
-                  onClick={() => handleAnswer(option)}
-                  className={
-                    isCorrectAnswer ? 'correct-answer' : 'answer-options'
-                  }
-                >
-                  {option}
-                </li>
+        } else {
+          // If the data does not exist in Firebase make the API call
+          axios
+            .get(
+              `https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}&type=${type}`
+            )
+            .then((response) => {
+              const questionsWithUniqueIds = response.data.results.map(
+                (question) => ({ ...question, id: uuidv4() })
               );
-            })}
-          </ul>
-        </div>
+              // Save the data in Firebase
+              set(gameSessionRef, questionsWithUniqueIds)
+                .then(() => {
+                  // Set the local state with the fetched data
+                  setQuestions(questionsWithUniqueIds);
+                  setLoading(false);
+                })
+                .catch((error) => {
+                  setError(
+                    'Failed to save questions to Firebase. Please try again later.'
+                  );
+                  setLoading(false);
+                });
+            })
+            .catch((error) => {
+              setError('Failed to fetch questions. Please try again later.');
+              setLoading(false);
+            });
+        }
+      })
+      .catch((error) => {
+        setError('Failed to fetch data from Firebase. Please try again later.');
+        setLoading(false);
+      });
+  };
 
-        {currentQuestion === questions.length - 1 ? (
-          <Link to={`/gameroom/${gameId}/result`}>View Score!</Link>
-        ) : null}
-        <Link to="/">Quit Game</Link>
+  // Rendering based on different states
+  if (loading) {
+    return (
+      <div className="spinnerContainer">
+        <PacmanLoader color="#fff" size={50} />
       </div>
     );
-  };
+  }
+
+  if (error) {
+    return (
+      <div>
+        {error}
+        <button onClick={() => fetchData(category, difficulty, type)}>
+          Retry
+        </button>
+      </div>
+    );
+  }
+  // Rendering based on different states
+  if (questions.length === 0) {
+    return <p>Loading...</p>;
+  }
+
+  const currentQuestionObj = questions[currentQuestion];
+  const options = [
+    ...currentQuestionObj.incorrect_answers,
+    currentQuestionObj.correct_answer,
+  ];
+  //Its going to shuffle options.We can call the sort() method, which accepts a function that returns a value between -0.5 and 0.5
+  //// options.sort(() => Math.random() - 0.5);
+
+  return (
+    <div>
+      <div className="quizInfo">
+        <p>Score: {score}/10</p>
+        <h3>{questions[currentQuestion].category}</h3>
+        <CountdownTimer
+          key={currentQuestion}
+          initialCount={15}
+          onExpire={handleExpire}
+          onReset={handleReset}
+          paused={paused}
+        />
+      </div>
+      <div className="questionFromAPI">
+        <h3>Question {currentQuestion + 1}</h3>
+        <p dangerouslySetInnerHTML={{ __html: currentQuestionObj.question }} />
+        <ul>
+          {options.map((option) => {
+            const optionId = uuidv4();
+            const isCorrectAnswer =
+              highlightAnswer && option === currentQuestionObj.correct_answer;
+            return (
+              <li
+                key={optionId}
+                onClick={() => handleAnswer(option)}
+                className={
+                  isCorrectAnswer ? 'correct-answer' : 'answer-options'
+                }
+              >
+                {option}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      {currentQuestion === questions.length - 1 ? (
+        <Link to={`/gameroom/${gameId}/result`}>View Score!</Link>
+      ) : null}
+      <Link to="/">Quit Game</Link>
+    </div>
+  );
 };
+
 export default GameRoom;
