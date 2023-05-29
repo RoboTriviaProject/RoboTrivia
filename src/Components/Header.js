94f1e0574c6c09762cd1f28c1240840ed388ae5d
noelle7 <noellehuang07@gmail.com>
2023-05-28 23:01:58 -0400

    finish styling home, pop up and welcome pages.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,29 @@
 import '../App.css';
 import logo from './../assets/images/robotLogo-min.png';
 import helpIcon from './../assets/images/helpIcon-min.png'
+import HowTo from './HowTo';
+import { useState } from 'react';
 
 const Header = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handlePopUp = () => {
+        setIsOpen(!isOpen);
+    };
+
+    
+
     return(
         <header>
             <img src={logo} className="logo" alt="a yellow robot head" />
             <h1>Robo Trivia</h1>
-            <img src={helpIcon} className="helpIcon" alt="circular help icon " />
+            <img src={helpIcon} className="helpIcon" alt="circular help icon " onClick={handlePopUp}/>
+            {
+                isOpen && <HowTo 
+                    handleClose={handlePopUp}
+                /> 
+            }
         </header>
     )
 }
--- src/Components/HowTo.js
+++ src/Components/HowTo.js
@@ -1,27 +1,37 @@
 import redRobot from './../assets/images/redRobot-min.png';
 import './../App.css';
 
-const HowTo = () => {
+const HowTo = (props) => {
+    
     return (
-        <section>
-            <h2>How to play</h2>
+        <section className='howContainer'>
             <div className="instructionContainer">
-                <span>01.</span>
-                <p>Choose your categories, level of difficulty and type of questions.</p>
-
-                <span>02.</span>
-                <p>Answer each question within 15 seconds.</p>
-
-                <span>03.</span>
-                <p>The one who gets the most points win.</p>
-
-                <span>04.</span>
-                <p>Have fun!</p>
+            {/* <button className='buttonClose'>✖</button> */}
+            <h2>How to play</h2>
+                <ul>
+                    <li>
+                        <span>01. </span>
+                        <p>Choose your categories, level of difficulty and type of questions.</p>
+                    </li>
+                    <li>
+                        <span>02.</span>
+                        <p>Answer each question within 15 seconds.</p>
+                    </li>
+                    <li>
+                        <span>03.</span>
+                        <p>The one who gets the most points win.</p>
+                    </li>
+                    <li>
+                        <span>04.</span>
+                        <p>Have fun!</p>
+                    </li>
+                </ul>
             </div>
             <div className="imageContainer">
                 <img src={redRobot} className="redRobotImg" alt="a red robot standing" />
             </div>
-            
         </section>
     )
 }
+
+export default HowTo;
